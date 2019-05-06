a3080c5148c7cd9fb3f03ddf050a98d451d4cedc
Angel Hernandez <Angelh2m@icloud.com>
2019-05-06 14:38:42 -0500

    Add files via upload

--- rest/src/main/java/com/app/rest/service/impl/UserServiceImpl.java
+++ rest/src/main/java/com/app/rest/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.app.rest.UserRepository;
 import com.app.rest.io.UserEntity;
 import com.app.rest.service.UserService;
 import com.app.rest.shared.UserDto;
+import com.app.rest.utils.Utils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,14 +15,23 @@ public class UserServiceImpl  implements UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    Utils utils;
+
     @Override
     public UserDto createUser(UserDto user) {
+
+        // Query the database and check id there is already an email
+        UserEntity usersEmail = userRepository.findByEmail(user.getEmail());
+        if (usersEmail != null) throw new RuntimeException("Record already exists");
+
         UserEntity userEntity = new UserEntity();
 
         BeanUtils.copyProperties(user, userEntity);
 
+        String publicUserId = utils.generateUserId(30);
         userEntity.setEncryptedPassword("Test");
-        userEntity.setUserId("TestUserId");
+        userEntity.setUserId(publicUserId);
         userEntity.setGetEmailVerificationStatus(false);
 
         UserEntity storedUserDetails = userRepository.save(userEntity);
